d471803b9c6cd896bfe48c2ed3da845a2abdbbd0
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-23 15:15:15 -0500

    Add schema-evolution-manager wrapper scripts

---
+++ dev.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# == Wrapper script to update a local postgrseql database
+#
+# == Usage
+#  ./dev.rb
+#
+
+Dir.chdir(File.dirname($0)) {
+  command = "sem-apply --url postgresql://api@localhost/splashpage"
+  puts command
+  exec(command)
+}
