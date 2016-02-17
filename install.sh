b720c946753d9f3ed6981e097427c97e4c88abb9
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-17 11:04:53 -0500

    Update to latest psql standards

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-psql -U postgres -c 'create database "splashpage"' postgres
+psql -U postgres -c 'create database splashpagedb' postgres
 psql -U postgres --no-align --tuples-only -c "SELECT 1 FROM pg_roles WHERE rolname='api'" | grep -q 1 || psql -U postgres -c 'create role api login PASSWORD NULL' postgres
-psql -U postgres -c 'GRANT ALL ON DATABASE "splashpage" TO api' postgres
-sem-apply --url postgresql://api@localhost/splashpage
+psql -U postgres -c 'GRANT ALL ON DATABASE splashpagedb TO api' postgres
+sem-apply --url postgresql://api@localhost/splashpagedb
