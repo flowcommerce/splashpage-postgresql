5fd8de3c5936ce0459763d23cc6cd1e348c73562
Michael Bryzek <mbryzek@alum.mit.edu>
2016-03-17 11:59:16 -0400

    Refresh journaling

--- scripts/20160317-114711.sql
+++ scripts/20160317-114711.sql
@@ -1,3 +1,5 @@
 alter table subscriptions drop latitude;
 alter table subscriptions drop longitude;
 alter table subscriptions add country text check(util.null_or_lower_non_empty_trimmed_string(country));
+
+select journal.refresh_journaling('public', 'subscriptions', 'journal', 'subscriptions');
